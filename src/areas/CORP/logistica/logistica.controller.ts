5007ebfc213b24e9ee02380619f948e364e005bf
Samir Priego <samir.apriego@gmail.com>
2024-05-20 15:11:19 -0600

    Manejo de viajes
    Logistica puede ahora Crear y OCnsultar viajes

--- src/areas/CORP/logistica/logistica.controller.ts
+++ src/areas/CORP/logistica/logistica.controller.ts
@@ -1,14 +1,46 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
 import ConsultaViajeDTO from 'src/Servicios/Servicos_Desarrollo/viajes/vehiculo/DTOS/viaje.filtros.dto';
 import { LogisticaService } from './logistica.service';
+import { UseInterceptors } from '@nestjs/common';
+import { CorpOneInterceptor } from 'src/interceptors/corp.interceptor';
+import { CreditoCobranzaService } from '../credito.cobranza/credito.cobranza.service';
+import { fns } from 'src/estructuras_y_algoritmos/funciones';
+import { FacturacionService } from '../facturacion/facturacion.service';
+import ViajeSalidaDTO from '../facturacion/DTOs/viaje.salida.dto';
+import { ViajesService } from 'src/Servicios/Servicos_Desarrollo/viajes/viajes.service';
 
 @Controller('logistica')
 export class LogisticaController {
     constructor(
-    private serVlogistica:LogisticaService
+        private serLGT:LogisticaService,
+        private serCYC:CreditoCobranzaService,
+        private serFCT:FacturacionService,
+        private serViajes:ViajesService
     ){}
+
+    @UseInterceptors(CorpOneInterceptor)
+    @Get('/documento/:empresa/:serie/:folio')
+    async getDocumento(@Param('empresa') empresa:'cdc'|'cmp', @Param('serie') serie:string, @Param('folio') folio:number){
+        return await fns.validar(async ()=>{
+            const builder = await this.serCYC.getOne(empresa, serie, folio);
+            const observacion = this.serCYC.getCometario({serie,folio:folio.toString()})
+            const factura = await builder.getOne;
+            return observacion!==null?{...factura, observacion}:factura;
+        },empresa)
+    }
+    //#region viajes
     @Get('viajes/consulta')
     async getViajes(@Query() query:ConsultaViajeDTO){
-        return await this.serVlogistica.getViajes(query)
+        return await this.serLGT.getViajes(query)
     }
+    @Get('/viaje/head/:empresa')
+    async getViajeHead(@Param('empresa') empresa:'cdc'|'cmp'){
+        return await this.serViajes.getHead(empresa);
+    }
+    @Post('/viajes/crear/:empresa')
+    async createViaje(@Body() body:ViajeSalidaDTO){
+        return await this.serFCT.crearViaje(body);
+    }
+    //#endregion
+
 }
--- src/areas/CORP/logistica/logistica.module.ts
+++ src/areas/CORP/logistica/logistica.module.ts
@@ -8,20 +8,45 @@ import { BitacoraModule } from 'src/Servicios/Servicos_Desarrollo/bitacora/bitac
 import entidadesList from 'src/entidades/entidadesCORP';
 import entidadesViajes from 'src/entidades/entidadesDesarrollos/Viajes';
 import entidadesCuentas from 'src/entidades/entidadesDesarrollos/Cuentas';
+import { CreditoCobranzaService } from '../credito.cobranza/credito.cobranza.service';
+import { DocumentosService } from 'src/Servicios/Servicos_CORP/documentos/documentos.service';
+import { ClientesService } from 'src/Servicios/Servicos_CORP/clientes/clientes.service';
+import { DomiciliosService } from 'src/Servicios/Servicos_CORP/domicilios/domicilios.service';
+import { VentasService } from 'src/Servicios/Servicos_Desarrollo/ventas/ventas.service';
+import { BitacoraService } from 'src/servicios/servicos_desarrollo/bitacora/bitacora.service';
+import entidadesBitacora from 'src/entidades/entidadesDesarrollos/Bitacora';
+import { AgenteDesService } from 'src/servicios/servicos_desarrollo/agente-des/agente-des.service';
+import { ChoferService } from 'src/Servicios/Servicos_Desarrollo/viajes/chofer/chofer.service';
+import { VehiculoService } from 'src/Servicios/Servicos_Desarrollo/viajes/vehiculo/vehiculo.service';
+import { FacturaCerradaService } from 'src/Servicios/Servicos_Desarrollo/viajes/factura_cerrada/factura_cerrada.service';
+import { FacturacionService } from '../facturacion/facturacion.service';
 
 @Module({
   imports:[
     TypeOrmModule.forFeature(entidadesList,'cdc'),
     TypeOrmModule.forFeature(entidadesList,'cmp'),
+    TypeOrmModule.forFeature([...entidadesBitacora],'bitacora'),
     TypeOrmModule.forFeature([...entidadesViajes],'viajes'),
     TypeOrmModule.forFeature([...entidadesCuentas],'cuentas'),
-    BitacoraModule
+    BitacoraModule,
   ],
   providers: [
     LogisticaService,
+    CreditoCobranzaService, 
+    DocumentosService, 
+    ClientesService, 
+    DomiciliosService,
+    VentasService,
+    UsuariosService,
+    BitacoraService,
+    ChoferService,
+    VehiculoService,
     ViajesService,
-    UsuariosService
+    AgenteDesService,
+    FacturaCerradaService,
+    FacturacionService
   ],
-  controllers: [LogisticaController]
+  controllers: [LogisticaController],
+  exports:[BitacoraService],
 })
 export class LogisticaModule {}
